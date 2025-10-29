d9781da27e8b1c3d7c28e76637608b0e1eb22606
Edmilson Constantino <edmilsonconstantino879@gmail.com>
2025-10-29 22:01:39 +0200

    atualizacao

--- certifications/serializers.py
+++ certifications/serializers.py
@@ -1,24 +1,33 @@
 from rest_framework import serializers
-from .models import Certification
+from .models import Certification, Modulo
+
+
+class ModuloSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Modulo
+        fields = ['id', 'nome'] 
 
 class CertificationSerializer(serializers.ModelSerializer):
     foto = serializers.SerializerMethodField()
     link_completo = serializers.SerializerMethodField()
 
+    modulos = ModuloSerializer(many=True, read_only=True)
+
     class Meta:
         model = Certification
-        fields = '__all__'
+        fields = '__all__' 
 
     def get_foto(self, obj):
+
         request = self.context.get("request")
         if obj.foto and request:
             return request.build_absolute_uri(obj.foto.url)
         elif obj.foto:
             return obj.foto.url
         return None
-    
+
     def get_link_completo(self, obj):
+       
         if obj.unique_link:
-            # ALTERE para o domínio do seu frontend
-            return f"https://www.cptec.co.mz/declaracoes{obj.unique_link}"
+            return f"https://www.cptec.co.mz/declaracoes/{obj.unique_link}"
         return None
