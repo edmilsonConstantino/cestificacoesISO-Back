544652a689db8bcfa73091483e54a6dd99070a91
Edmilson Constantino <edmilsonconstantino879@gmail.com>
2025-11-07 10:59:06 +0200

    atuali

--- certifications/models.py
+++ certifications/models.py
@@ -89,7 +89,7 @@ class Certification(models.Model):
         unique=True,
         blank=True,
         null=True,
-        editable=True,
+        editable=False,
         verbose_name="Link Único de Compartilhamento",
         help_text="Gerado automaticamente ao salvar",
         db_index=True
